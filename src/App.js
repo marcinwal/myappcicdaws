fc74b5f4b536383c6385c0b9b859464d5a77e1b2
marcin.walendzik@gmail.com <marcin.walendzik@gmail.com>
2022-05-23 15:36:19 +0200

    minor change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>A new version of CI/CD react project</h1>
+        <h1>A newer version of CI/CD react project</h1>
         <h2>We now have Auth!</h2>
       </header>
       <AmplifySignOut />
